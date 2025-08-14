7bd0512e3e8d45e09f908247f5f1e5bcb5fd0f84
Aboje Edwin <abojeedwin@gmail.com>
2025-08-14 14:29:26 +0100

    Resolving files

--- football-talent-platform/components/upload/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ProfileUploadModal } from "./profile-upload-modal"
-export { default as ImageUpload } from "./image-upload"
-export { default as UploadProgress } from "./upload-progress"
-export { default as BulkUpload } from "./bulk-upload"
